3f713ce5ede3b987d01b22f2d487dd1aabe462ff
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-05-22 16:46:29 -0400

    Started Angular Routes

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ var mongoose = require ('./db/schema.js')
 var Question = mongoose.model('Question')
 var Answer = mongoose.model('Answer')
 
+app.get('/', (req, res) => {
+  res.send("It was the best of times. It was the worst of times.");
+})
+
+// app.get('')
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000, function(){
     console.log ("listening on port 3000")
 })
--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,36 @@ angular
     "$stateProvider",
     Router
   ])
+  .factory('Book', [
+    '$resource',
+      Book
+  ])
+  .controller('indexCtrl', [
+    "Book",
+    indexController
 
+  ])
   function Router ($stateProvider) {
     $stateProvider
     .state('welcome', {
       url: '/',
-      templateURL: '/public/js/ng-views/welcome.html'
+      templateURL: '/public/js/ng-views/welcome.html',
+      controller: 'indexCtrl',
+      controllerAs:'vm'
     })
   }
+  .state('index', {
+    url:'/questions',
+    templateURL:'/public/js/ng-views/index.html'
+  })
+}
+
+   function Q ($resource) {
+    return $resource("/api/candidates/:name", {}, {
+      update: { method: "PUT" }
+    });
+   }
+
+    function indexController (Candidate) {
+      this.candidates = Candidate.query()
+    }
